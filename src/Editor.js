f6ce71fd2c3238824a88e6aad58a07cd52a66a4d
Joshua Yoes <>
2020-01-01 20:15:22 -0800

    Setup editor and previewer styles

---
+++ src/Editor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Editor() {
+    return (
+        <textarea id="editor">
+            This is some markdown
+        </textarea>
+    )
+}
---
+++ src/Previewer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Previewer() {
+    return (
+        <section id="preview">
+            This is the Previewer
+        </section>
+    )
+}
