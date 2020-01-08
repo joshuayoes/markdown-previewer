4a8c4414769831e74e6ab4bb403ceddb9ff83770
Joshua Yoes <>
2020-01-07 20:57:12 -0800

    Added ability to download markdown files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import { MdWeb } from 'react-icons/md'
+import React, {useState} from 'react'
+import { connect } from 'react-redux'
+import { MdWeb, MdFileDownload } from 'react-icons/md'
 import { FaGithub, FaTwitter } from 'react-icons/fa'
+import FileSaver from 'file-saver'
 
-export default function Header() {
+
+const Header = ({ markdown }) => {
+    const [markdownTitle, setMarkdownTitle] = useState("");
+
+    const saveMarkdown = () => {
+        const markdownTitleFallback = (markdownTitle === "") ? "README" : markdownTitle;
+        const markdownBlob = new Blob([markdown], {type: "text/markdown; charset=UTF-8"});
+        FileSaver.saveAs(markdownBlob, `${markdownTitleFallback}.md`);
+    }
+
     return (
         <header>
             <h1 className="home">Markdown Previewer</h1>
@@ -18,6 +29,26 @@ export default function Header() {
                     <FaTwitter size={32} />
                 </a>
             </div>
+            <div className="file">
+                <input
+                    type="text"
+                    placeholder={"README"}
+                    value={markdownTitle}
+                    onChange={(event) => setMarkdownTitle(event.target.value)}
+                />
+                <p>.md</p>
+                <button onClick={saveMarkdown}>
+                    <MdFileDownload size={26} />
+                    Download
+                </button>
+            </div>
         </header>
     )
 }
+
+const mapStateToProps = (state) => {
+    const { markdown } = state
+    return { markdown }
+}
+
+export default connect(mapStateToProps)(Header);
